f2a0ed72370d64d5ec6ee977987271ac7d3eb6ac
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-11-29 12:35:15 +0530

    working on tabes

--- src/Pages/Spot/index.jsx
+++ src/Pages/Spot/index.jsx
@@ -3,9 +3,10 @@ import { Box, Typography } from '@mui/material'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { sBitIcon } from "../../Assets/Data"
 
-
-
 import "./style.css"
+
+//images
+import refreshLogo from "../../Assets/Images/image 124.png.svg"
 
 //component
 import NavBar from '../../Components/NavBar'
@@ -20,8 +21,6 @@ export default function Spot() {
       <Box className="spotContainer">
 
         <Box className="spotFBox">
-
-
           <Box className="spotSideMenu">
             {/* side menu */}
             <Box className="spotNavMenu">
@@ -36,29 +35,217 @@ export default function Spot() {
               </Box>
             </Box>
 
-            {/* tabe section */}
-            <Box className="tabeinnerSection market">
+            {/* tabe  marketSection */}
+            <Box sx={{ display: sideTab === 1 ? "flex" : "none" }} className="tabeinnerSection marketSection">
               <Typography className='sideMenuGText'>Pay</Typography>
+
               <Box className="usdTHeader">
                 <Box className="usdSelectorBox">
                   {sBitIcon}
-
+                  <Typography>USDT</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
                 </Box>
-
                 <Box className="usdTBox">
                   <Box><Typography>25</Typography></Box>
                   <Box><Typography>155</Typography></Box>
                 </Box>
               </Box>
 
+              <Box className="usdTHeaderValue">
+                <Box className="usdTHeaderValueItem">
+                  <Typography>25</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>50</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>75</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>MAX</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem customBtn">
+                  <Typography>Custom</Typography>
+                </Box>
+              </Box>
 
+              <Box className="placeholderRefres_box">
+                <img src={refreshLogo} />
+              </Box>
+              <Typography className='sideMenuGText'>Receive</Typography>
+
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  {sBitIcon}
+                  <Typography>USDT</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox RusdTBox">
+                  <Typography>27.8377</Typography>
+                </Box>
+              </Box>
+              <Box className="buyBtn">
+                <Typography>Buy</Typography>
+              </Box>
             </Box>
 
 
 
 
-          </Box>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/*Table  LimitSection */}
+            <Box sx={{ display: sideTab === 2 ? "flex" : "none" }} className="tabeinnerSection">
+              <Typography className='sideMenuGText'>Paid token</Typography>
+
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  {sBitIcon}
+                  <Typography>MATIC</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox">
+                  <Box><Typography>1.45</Typography></Box>
+                  <Box><Typography>25$</Typography></Box>
+                </Box>
+              </Box>
+
+              <Box className="usdTHeaderValue">
+                <Box className="usdTHeaderValueItem">
+                  <Typography>25</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>50</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>75</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>MAX</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem customBtn">
+                  <Typography>Custom</Typography>
+                </Box>
+              </Box>
+
+              <Typography className='sideMenuGText'>Expiry</Typography>
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  <Typography>1 hour</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox">
+                  <Box><Typography>1.45</Typography></Box>
+                  <Box><Typography>25$</Typography></Box>
+                </Box>
+              </Box>
+
+              <Box className="placeholderRefres_box">
+                <img src={refreshLogo} />
+              </Box>
+              <Typography className='sideMenuGText'>You Get</Typography>
+
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  {sBitIcon}
+                  <Typography>BTC</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox RusdTBox">
+                  <Typography>27.8377</Typography>
+                </Box>
+              </Box>
+              <Box className="buyBtn">
+                <Typography>Buy</Typography>
+              </Box>
+            </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/*Table  StopMarketSection */}
+            <Box sx={{ display: sideTab === 3 ? "flex" : "none" }} className="tabeinnerSection">
+              <Typography className='sideMenuGText'>Pay</Typography>
+
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  {sBitIcon}
+                  <Typography>USDT</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox">
+                  <Box><Typography>25</Typography></Box>
+                  <Box><Typography>155</Typography></Box>
+                </Box>
+              </Box>
+
+              <Box className="usdTHeaderValue">
+                <Box className="usdTHeaderValueItem">
+                  <Typography>25</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>50</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>75</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem">
+                  <Typography>MAX</Typography>
+                </Box>
+                <Box className="usdTHeaderValueItem customBtn">
+                  <Typography>Custom</Typography>
+                </Box>
+              </Box>
+
+              <Box className="placeholderRefres_box">
+                <img src={refreshLogo} />
+              </Box>
+              <Typography className='sideMenuGText'>Receive</Typography>
+
+              <Box className="usdTHeader">
+                <Box className="usdSelectorBox">
+                  {sBitIcon}
+                  <Typography>USDT</Typography>
+                  <KeyboardArrowDownIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box className="usdTBox RusdTBox">
+                  <Typography>27.8377</Typography>
+                </Box>
+              </Box>
+              <Box className="buyBtn">
+                <Typography>Buy</Typography>
+              </Box>
+            </Box>
+          </Box>
 
 
 
